397be150f5dbee1e75abd5a14a22ab0948a4aced
Thomas Monfre <tmonfre@users.noreply.github.com>
2019-04-10 02:49:03 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+// recursive function to display a count on the screen for seconds spent on site
+function displayCount(num) {
+	// eslint-disable-next-line no-tabs
+	setTimeout(() => {
+		// display text on screen and increment count
+		$('#main').text(`You've been on this page for ${num} seconds.`);
+
+		// make recursive call
+		displayCount(num + 1);
+	}, 1000);
+}
+
+displayCount(1);
