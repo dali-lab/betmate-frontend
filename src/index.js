baf5acee277529f11c96275cee2937cd948a510f
Thomas Monfre <tmonfre@users.noreply.github.com>
2019-04-23 20:39:30 -0400

    added react router support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
 import './style.scss';
 
-const App = () => <div className="test">All the REACT belong to us!</div>;
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -8,66 +8,72 @@ const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { 
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-	mode: env,
-	entry: ['babel-polyfill', './src'], // this is where our app lives
-	devtool: 'source-map', // this enables debugging with source in chrome devtools
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: [
-					{ loader: 'babel-loader' },
-					{ loader: 'eslint-loader' },
-				],
-			},
-			{
-				test: /\.s?css/,
-				use: [
-					finalCSSLoader,
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: true,
-						},
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							plugins: () => [autoprefixer()],
-							sourceMap: true,
-						},
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							sourceMap: true,
-						},
-					},
-				],
-			},
-			{
-				test: /\.(jpe?g|png|gif|svg)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							useRelativePath: true,
-							name: '[name].[ext]',
-						},
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new MiniCssExtractPlugin(),
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-			filename: './index.html',
-		}),
-	],
-	devServer: {
-		hot: true,
-	},
+  mode: env,
+  output: { publicPath: '/' },
+  entry: ['babel-polyfill', './src'], // this is where our app lives
+  devtool: 'source-map', // this enables debugging with source in chrome devtools
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          { loader: 'eslint-loader' },
+        ],
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          finalCSSLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()],
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './200.html',
+    }),
+  ],
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+  },
 };
