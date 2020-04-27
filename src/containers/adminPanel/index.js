a40a9529c642a0ecda9512358264a2b0e8f5c997
alejandro <lopez.alejandro@dali.dartmouth.edu>
2020-04-27 18:25:17 -0400

    stuck-authorization-buttons-updated

--- src/containers/adminPanel/index.js
+++ src/containers/adminPanel/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { fetchResources, fetchResourceByID, deleteResourceByID } from '../../actions/resourceActions';
+import { fetchUsers, fetchUserByID, deleteUserByID } from '../../actions/userActions';
 import SearchItem from '../../components/SearchItem';
 
-
-function getUserById(id) {
-  // gets user by id
-  console.log(id);
-}
 
 class AdminPanel extends React.Component {
   constructor(props) {
@@ -16,28 +12,25 @@ class AdminPanel extends React.Component {
     this.state = {
       user_id_get: '',
       resource_id_get: '',
-      // users: 'no users',
-      // resources: 'no resources',
-      // current_user: 'no current_user',
-      // current_resource: 'no current resource',
 
     };
 
-    // this.getAllUsers = this.getAllUsers.bind(this);
+    this.getAllUsers = this.getAllUsers.bind(this);
+    this.getUserById = this.getUserById.bind(this);
+    this.deleteUser = this.deleteUser.bind(this);
     this.getAllResources = this.getAllResources.bind(this);
     this.getResourceById = this.getResourceById.bind(this);
-    // this.deleteUser = this.deleteUser.bind(this);
     this.deleteResource = this.deleteResource.bind(this);
   }
 
-  // getAllUsers() {
-  //   // gets all users from server
-  //   console.log('get all users');
-  //   // this.setState({ users: 'users should be displayed' });
-  //   // return (
-  //   //   <div>{this.state.users} </div>
-  //   // );
-  // }
+  getAllUsers() {
+    this.props.fetchUsers();
+  }
+
+  getUserById(id) {
+    this.props.fetchUserByID(id);
+    console.log(id);
+  }
 
   getAllResources() {
     // gets all resources from server
@@ -50,13 +43,14 @@ class AdminPanel extends React.Component {
     console.log(id);
   }
 
-  // deleteUser(id) {
-  //   // deletes user
-  // }
-  //
+  deleteUser(id) {
+    this.props.deleteUserByID(id, {});
+    console.log(id);
+  }
+
   deleteResource(id) {
     // deletes resource
-    this.props.deleteResourceByID(id);
+    this.props.deleteResourceByID(id, {});
     console.log(id);
   }
 
@@ -69,8 +63,8 @@ class AdminPanel extends React.Component {
         <button type="button" onClick={this.getAllUsers}>Get all users</button>
 
         <input type="text" value={this.state.user_id_get} onChange={e => this.setState({ user_id_get: e.target.value })} />
-        <button type="button" onClick={e => getUserById(this.state.user_id_get)}> Submit </button>
-        <button type="button" onClick={this.deleteUser}>Delete user</button>
+        <button type="button" onClick={e => this.getUserById(this.state.user_id_get)}> Submit </button>
+        <button type="button" onClick={e => this.deleteUser(this.state.user_id_get)}>Delete user</button>
         <br />
 
         <button type="button" onClick={this.getAllResources}>Get all resources</button>
@@ -81,12 +75,34 @@ class AdminPanel extends React.Component {
         <br />
 
 
+        <p> Resources: </p>
+        <br />
+
         <div> {this.props.results && this.props.results.length ? this.props.results.map((element) => {
           return <SearchItem key={element.id || element._id} displayObject={element} />;
         }) : null}
         </div>
 
+        <p> Selected resource: </p>
+        <br />
+
         <div> {this.props.resource ? <SearchItem key={this.props.resource.id || this.props.resource._id} displayObject={this.props.resource} />
+          : null}
+        </div>
+        <br />
+
+        <p> Users: </p>
+        <br />
+
+        <div> {this.props.users && this.props.users.length ? this.props.users.map((element) => {
+          return <SearchItem key={element.id || element._id} displayObject={element} />;
+        }) : null}
+        </div>
+
+        <p> Selected user: </p>
+        <br />
+
+        <div> {this.props.user ? <SearchItem key={this.props.user.id || this.props.user._id} displayObject={this.props.user} />
           : null}
         </div>
 
@@ -99,6 +115,10 @@ class AdminPanel extends React.Component {
 const mapStateToProps = state => ({
   results: state.data.resources,
   resource: state.data.resource,
+  users: state.data.users,
+  user: state.data.user,
 });
 
-export default connect(mapStateToProps, { fetchResources, fetchResourceByID, deleteResourceByID })(AdminPanel);
+export default connect(mapStateToProps, {
+  fetchResources, fetchResourceByID, deleteResourceByID, fetchUsers, fetchUserByID, deleteUserByID,
+})(AdminPanel);
