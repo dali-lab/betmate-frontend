3486e5633cea955383e914338ef806fc86833f79
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-10-15 03:04:26 -0400

    Documentation commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,14 +16,40 @@ const ActionTypes = {
   DELETE_USER: 'DELETE_USER',
 };
 
+/**
+* Generates valid "success" payload given a response object and additional custom parameters
+* * Note: Use this whenever not using `createAsyncActionCreator` to correctly interface with reducers
+* @param {*} response - Axios Response
+* @param {*} customParams - Any additional parameters to inject into action.payload
+*/
 export function generateSuccessPayload(response, customParams = {}) {
   return ({ data: response.data, code: response.status, ...customParams });
 }
 
+/**
+ * Generates valid "failure" payload given an error object and additional custom parameters
+ * * Note: Use this whenever not using `createAsyncActionCreator` to correctly interface with reducers
+ */
+
+/**
+ * Generates valid "failure" payload given an error object and additional custom parameters
+ * * Note: Use this whenever not using `createAsyncActionCreator` to correctly interface with reducers
+ * @param {*} error - Javascript error object
+ * @param {*} customParams - Any additional parameters to inject into action.payload
+ */
 export function generateFailurePayload(error, customParams = {}) {
   return ({ message: error.response?.data?.message || error.message || 'No message found', code: error.response?.status || error.code || null, ...customParams });
 }
 
+/**
+ * A function which standardizes the creation of asynchronous action creators. This allows for standardization in the creation and maintenance of reducers.
+ * * Note: This function is intended to reduce boilerplate code. If additional customization is needed, see `generateSuccessPayload` and `generateFailurePayload`
+ * @param {*} dispatch - Redux dispatch function
+ * @param {*} actionName - Name of action to manage (e.g. ActionTypes.FETCH_USER)
+ * @param {*} axiosConfig - Standard axios configuration object
+ * @param {*} successCallback - Optional callback called after success dispatch
+ * @param {*} failureCallback - Optional callback called after failure dispatch
+ */
 export async function createAsyncActionCreator(dispatch, actionName, axiosConfig, successCallback = () => {}, failureCallback = () => {}) {
   try {
     dispatch({ type: `${actionName}_REQUEST` });
