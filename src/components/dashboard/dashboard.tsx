6b99612472756726cb11360d4b451344e6921084
Benedict Tedjokusumo <bentaro89@gmail.com>
2021-04-27 20:14:55 -0400

    added basic card structure

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './dashboard.scss';
+import GameCard from './gameCard';
+import filler from './currentGamesDummy';
 
 const Dashboard = () => {
   return (
@@ -13,11 +15,20 @@ const Dashboard = () => {
         <div className='browse-button'>
           <button>Browse</button>
         </div>
-
       </div>
-      <div>
-        <h3 className='betting-header'>continue betting</h3>
-      </div>
+      <h3 className='betting-header'>continue betting</h3>
+      {filler.map((i) => (
+        <div key='key' className='card-box'>
+          <GameCard
+            gameID={i.gameID}
+            player1={i.player1}
+            player2={i.player2}
+            player1Rating={i.player1Rating}
+            player2Rating= {i.player2Rating}
+            playerFavor={i.playerFavor}
+            earnings={i.earnings}/>
+        </div>
+      ))}
     </div>
 
   );
