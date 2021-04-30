5849170835156b8782d003c5f362cb55b02d4d3d
Benedict Tedjokusumo <bentaro89@gmail.com>
2021-04-30 17:32:31 -0400

    Added chessboard and added logic with chess.js

--- src/components/chessMatch/chessMatch.tsx
+++ src/components/chessMatch/chessMatch.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import Chessground from 'react-chessground';
+/* eslint-disable @typescript-eslint/no-var-requires */
+import React, { useState } from 'react';
 import 'react-chessground/dist/styles/chessground.css';
 import './chessMatch.scss';
+import Chessboard from 'chessboardjsx';
+// import Chess from 'chess.js';
+const Chess = require('chess.js');
 
-const ChessMatch = (props) => {
+const chess = new Chess();
+
+const ChessMatch = () => {
+  const [fen, updateFen] = useState('');
+  const clicky = () => {
+    const moves = chess.moves();
+    const move = moves[Math.floor(Math.random() * moves.length)];
+    chess.move(move);
+    // console.log(chess.fen());
+    updateFen(chess.fen());
+  };
   return (
     <div className='chess-board'>
-      <Chessground />
+      <button onClick={() => clicky()}/>
+      <Chessboard position={fen} />
     </div>
   );
 };
