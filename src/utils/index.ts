67c1cc3a99ee18b7bbda9b8fadde43bd4f25f4c5
faustino99 <faustino.cortina.21@dartmouth.edu>
2021-05-03 20:58:59 -0400

    created preprod and prod environments

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,10 @@
 // Server URL for making backend requests
-export const ROOT_URL = 'http://localhost:9090';
+// eslint-disable-next-line no-nested-ternary
+export const ROOT_URL = process.env.TARGET_ENV === 'prod'
+  ? 'https://betmate-backend-prod.herokuapp.com/'
+  : process.env.TARGET_ENV === 'dev'
+    ? 'https://betmate-backend-dev.herokuapp.com'
+    : 'http://localhost:9090';
 
 // Auth token name for storage and transmission to backend
 export const authTokenName = 'authToken';
