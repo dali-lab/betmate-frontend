7b65d0a195b9db4fbdd82319a03fc6a46d7f98e0
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-05-06 00:27:22 -0600

    fix: remove chess.js from chessMatch

--- src/components/chessMatch/component.tsx
+++ src/components/chessMatch/component.tsx
@@ -7,11 +7,6 @@ import { joinGame, leaveGame } from 'store/actionCreators/websocketActionCreator
 import { fetchGameById } from 'store/actionCreators/gameActionCreators';
 import { Game } from 'types/resources/game';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const Chess = require('chess.js');
-
-const chess = new Chess();
-
 interface ChessMatchProps extends RouteComponentProps<{ id: string }> {
   joinGame: typeof joinGame,
   leaveGame: typeof leaveGame,
@@ -21,7 +16,7 @@ interface ChessMatchProps extends RouteComponentProps<{ id: string }> {
 
 const ChessMatch: React.FC<ChessMatchProps> = (props) => {
   const gameId = props.match.params.id;
-  const [fen, updateFen] = useState(chess.fen());
+  const [fen, updateFen] = useState('');
 
   useEffect(() => {
     console.log(gameId);
@@ -35,15 +30,8 @@ const ChessMatch: React.FC<ChessMatchProps> = (props) => {
     if (game) updateFen(game.state);
   }, [props.games]);
 
-  const clicky = () => {
-    const moves = chess.moves();
-    const move = moves[Math.floor(Math.random() * moves.length)];
-    chess.move(move);
-    updateFen(chess.fen());
-  };
   return (
     <div className='chess-board'>
-      <button onClick={() => clicky()}/>
       <Chessboard position={fen} />
     </div>
   );
