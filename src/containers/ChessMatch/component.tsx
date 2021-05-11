7e034a5a357023bbde0bb44a2501f6d09b0bac9e
Benedict Tedjokusumo <bentaro89@gmail.com>
2021-05-11 15:18:32 -0400

    fixed player info

--- src/containers/ChessMatch/component.tsx
+++ src/containers/ChessMatch/component.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
 import Chessboard from 'chessboardjsx';
-import './style.scss';
-import { joinGame, leaveGame } from 'store/actionCreators/websocketActionCreators';
 import WagerPanel from 'components/WagerPanel';
 import NavBar from 'components/NavBar';
+import { joinGame, leaveGame } from 'store/actionCreators/websocketActionCreators';
+import { useParams } from 'react-router';
 import { fetchGameById } from 'store/actionCreators/gameActionCreators';
 import { Game } from 'types/resources/game';
+import playerIconBlack from 'assets/player_icon_black.svg';
+import playerIconWhite from 'assets/player_icon_white.svg';
+import './style.scss';
 
 interface ChessMatchProps {
   joinGame: typeof joinGame
@@ -40,25 +42,32 @@ const ChessMatch: React.FC<ChessMatchProps> = (props) => {
         </div>
         <div>
           <div className='player-info'>
-            <div>
-              <h3 className='player-name'>{props.games[gameId].player_black.name} </h3>
-              <p className='player-name'>({props.games[gameId].player_black.elo})</p>
+            <div className='player-title'>
+              <img className='player-icon' src={playerIconBlack}/>
+              <div>
+                <h3 className='player-name'>{props.games[gameId].player_black.name} </h3>
+                <p className='player-name'>({props.games[gameId].player_black.elo})</p>
+              </div>
             </div>
+            <div className='time-rect black-rect'>
+              <h3 className='player-time'>{new Date(props.games[gameId].time_black * 1000).toISOString().substr(14, 8)}</h3>
+            </div>          </div>
 
-            <p className='player-time'>{new Date(props.games[gameId].time_black * 1000).toISOString().substr(14, 8)}</p>
-            {console.log(props.games[gameId])}
-          </div>
           <div className='chessBoard'>
-            <Chessboard position={fen} width={700}/>
+            <Chessboard position={fen} width={600}/>
           </div>
-          <div className='player-info'>
-            <div>
-              <h3 className='player-name'>{props.games[gameId].player_white.name} </h3>
-              <p className='player-name'>({props.games[gameId].player_white.elo})</p>
-            </div>
 
-            <p className='player-time'>{new Date(props.games[gameId].time_white * 1000).toISOString().substr(14, 8)}</p>
-            {console.log(props.games[gameId])}
+          <div className='player-info'>
+            <div className='player-title'>
+              <img className='player-icon' src={playerIconWhite}/>
+              <div>
+                <h3 className='player-name'>{props.games[gameId].player_white.name} </h3>
+                <p className='player-name'>({props.games[gameId].player_white.elo})</p>
+              </div>
+            </div>
+            <div className='time-rect white-rect'>
+              <h3 className='player-time'>{new Date(props.games[gameId].time_white * 1000).toISOString().substr(14, 8)}</h3>
+            </div>
           </div>
         </div>
         <WagerPanel/>
