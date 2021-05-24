ce238bfe5d087e2e71855b02976537218df261be
Benedict Tedjokusumo <bentaro89@gmail.com>
2021-05-23 23:54:48 -0400

    timer counts down and changes format depending on amount of time

--- src/containers/ChessMatch/playerInfo/component.tsx
+++ src/containers/ChessMatch/playerInfo/component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.scss';
 
 interface ChessMatchProps {
@@ -10,6 +10,20 @@ interface ChessMatchProps {
 }
 
 const PlayerInfo: React.FC<ChessMatchProps> = (props) => {
+  // const playerTime = new Date((props.time ?? 0) * 1000).toISOString().substr(14, 8);
+  // const playerTime = props.time;
+  const [playerTime, setCounter] = useState(props.time == null ? 0 : props.time);
+
+  useEffect(() => {
+    let decrease = 0;
+    if (playerTime > 0) decrease = 0.01;
+    const timer = setInterval(() => setCounter(playerTime - decrease), 10);
+    return () => clearInterval(timer);
+  }, [playerTime]);
+
+  const timeString = new Date((playerTime ?? 0) * 1000).toISOString();
+  // new Date((props.time ?? 0) * 1000).toISOString().substr(14, 8);
+
   return (
     <div className='player-info'>
       <div className='player-title'>
@@ -20,7 +34,7 @@ const PlayerInfo: React.FC<ChessMatchProps> = (props) => {
         </div>
       </div>
       <div className={props.isBlack ? 'time-rect black-rect' : 'time-rect white-rect'}>
-        <h3 className='player-time'>{new Date((props.time ?? 0) * 1000).toISOString().substr(14, 8)}</h3>
+        <h3 className='player-time'>{playerTime > 60 ? timeString.substr(11, 8) : timeString.substr(14, 8) }</h3>
       </div>
     </div>
   );
