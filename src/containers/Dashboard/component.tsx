12c1b7aeeac4ba1c3ad15bdd95b0eec09ce05b7b
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-07-19 22:14:45 -0400

    feat: make match container single row

--- src/containers/Dashboard/component.tsx
+++ src/containers/Dashboard/component.tsx
@@ -81,25 +81,27 @@ const Dashboard: React.FC<DashboardProps> = (props) => {
         <Leaderboard />
       </div>
       <h3 className='betting-header'>Current Matches 👀</h3>
-      {props.games
-        .filter((g) => g !== topGame)
-        .sort((gameA, gameB) => new Date(gameB.created_at).getTime() - new Date(gameA.created_at).getTime())
-        .map((game, i) => {
-          const id = game._id;
-          return (
-            <div key={id} className={`card-box ${i % 2 ? 'pink' : 'orange'}`}>
-              <GameCard
-                gameID={id}
-                player1={'Black'}
-                player2={'White'}
-                player1Rating={game.player_black.elo}
-                player2Rating={game.player_white.elo}
-                playerFavor={getFavoredPlayer(game.odds)}
-                gameOdds={game.odds}
-              />
-            </div>
-          );
-        })}
+      <div className="match-container">
+        {props.games
+          .filter((g) => g !== topGame)
+          .sort((gameA, gameB) => new Date(gameB.created_at).getTime() - new Date(gameA.created_at).getTime())
+          .map((game, i) => {
+            const id = game._id;
+            return (
+              <div key={id} className={`card-box ${i % 2 ? 'pink' : 'orange'}`}>
+                <GameCard
+                  gameID={id}
+                  player1={'Black'}
+                  player2={'White'}
+                  player1Rating={game.player_black.elo}
+                  player2Rating={game.player_white.elo}
+                  playerFavor={getFavoredPlayer(game.odds)}
+                  gameOdds={game.odds}
+                />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
