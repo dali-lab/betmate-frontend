84066f63de196f4b0bf29ce505bdb77f7ca4ee27
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-07-21 22:16:01 -0400

    fix: adjust root url

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-nested-ternary
 export const ROOT_URL = process.env.TARGET_ENV === 'prod'
   ? 'https://betmate-backend-prod.herokuapp.com/'
-  : process.env.TARGET_ENV === 'dev' || true
+  : process.env.TARGET_ENV === 'dev'
     ? 'https://betmate-backend-dev.herokuapp.com'
     : 'http://localhost:9090';
 
