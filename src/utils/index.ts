2601a62a9f1d7545dc7894ab9d8ba7b26515b781
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-08-04 00:00:54 -0400

    fix: remove slash from production url

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@
 // Server URL for making backend requests
 // eslint-disable-next-line no-nested-ternary
 export const ROOT_URL = process.env.TARGET_ENV === 'prod'
-  ? 'https://betmate-backend-prod.herokuapp.com/'
+  ? 'https://betmate-backend-prod.herokuapp.com'
   : process.env.TARGET_ENV === 'dev'
     ? 'https://betmate-backend-dev.herokuapp.com'
     : 'http://localhost:9090';
