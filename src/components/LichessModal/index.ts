af222afb649c6abf81a2dfe57c8469d64eed3270
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-08-15 22:29:10 -0400

    fix: remove connect from lichess modal

--- src/components/LichessModal/index.ts
+++ src/components/LichessModal/index.ts
@@ -1,11 +1,4 @@
-import { connect } from 'react-redux';
-
-// import { RootState } from 'types/state';
-
 import LichessModal from './component';
 import './style.scss';
 
-const mapStateToProps = () => ({
-});
-
-export default connect(mapStateToProps, {})(LichessModal);
+export default LichessModal;
